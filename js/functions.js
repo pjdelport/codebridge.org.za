20d22ec157d197c8452bee5d160548585e6e648c
Guus Hoekman <mail@guushoekman.com>
2017-10-31 13:20:16 +0200

    better filter

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,13 @@
 var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1sN95ta5lfqeznMRJxx6F8Uub5BB-03t5W6VsIgS_9j0/pubhtml';
 
-$(document).ready( function() {
+$(document).ready(function() {
   Tabletop.init( { key: public_spreadsheet_url,
                    callback: showInfo,
                    parseNumbers: true } );
 });
   
 function showInfo(data, tabletop) {
-  var source   = $("#details").html();
+  var source = $("#details").html();
   var template = Handlebars.compile(source);
 
   $.each( tabletop.sheets("Details").all(), function(i, detail) {
@@ -18,19 +18,27 @@ function showInfo(data, tabletop) {
   $(".loading").hide();
 }
 
-$(document).ready( function() {
-  $("#filters :checkbox").prop("checked", false);
-  $("#showAll").prop("checked", true);
-  $("#filters :checkbox").not("#showAll").click(function() {
-    $("#showAll").prop("checked", false);
+$(document).ready(function() {
+  var visibleItems = [];
+  $("#filter input").prop("checked", false);
+  $("#province-filter .show-all").prop("checked", true);
+  $("#province-filter :checkbox").not(".show-all").click(function() {
+    $(".show-all").prop("checked", false);
   });
-  $("#filters #showAll").click(function() {
-    $("#filters :checkbox").not("#showAll").prop("checked", false);
+  $("#province-filter .show-all").click(function() {
+    $("#province-filter :checkbox").not(".show-all").prop("checked", false);
   });
-  $("#filters :checkbox").click(function() {
-    $(".well").hide();
-    $("#filters :checkbox:checked").each(function() {
-      $("." + $(this).val()).show();
-    });
+  $("#filter input").change(function () {
+    visibleItems = $("#filter input:checked").map(function (i) {
+      return $(this).val();
+    }).get();
+    if (visibleItems.length > 0) {
+      $(".single-item").hide();
+      $("." + visibleItems.join('.')).show();
+      console.log(visibleItems.length);
+    }
+    else {
+      $(".single-item").hide();
+    }
   });
 });
