1a5830c589038a819af797d3284720d37aa311df
Guus Hoekman <mail@guushoekman.com>
2018-03-06 11:29:11 +0200

    added tutorials

--- js/content.js
+++ js/content.js
@@ -80,5 +80,72 @@ function loadProjects() {
   };
 };
 
+// Tutorials
+function loadTutorials() {
+  var tutorialsDoc = 'https://docs.google.com/spreadsheets/d/1k3B8vgoYOUqrMwxUaGgCFIPua6gRBoraBQwdQlStMLc/pubhtml';
+
+  $(document).ready(function() {
+    Tabletop.init({
+      key: tutorialsDoc,
+      callback: showTutorials,
+      orderby: 'featured',
+      parseNumbers: false
+    });
+  });
+
+  function showTutorials(data, tabletop) {
+    var source = $("#tutorials-template").html();
+    var template = Handlebars.compile(source);
+
+    $.each( tabletop.sheets("Tutorials").all(), function(i, detail) {
+      var html = template(detail);
+      $("#tutorials").append(html);
+    });
+
+    $("#tutorials .col-item.approved").each(function() {
+      if ( $(this).index() < 6 ) {
+        $(this).addClass("visible");
+      }
+    });
+
+    // Find all YouTube videos
+    var $allVideos = $("iframe[src^='//www.youtube.com']"),
+
+        // The element that is fluid width
+        $fluidEl = $("body");
+
+    // Figure out and save aspect ratio for each video
+    $allVideos.each(function() {
+
+      $(this)
+        .data('aspectRatio', this.height / this.width)
+
+        // and remove the hard coded width/height
+        .removeAttr('height')
+        .removeAttr('width');
+
+    });
+
+    // When the window is resized
+    $(window).resize(function() {
+
+      var newWidth = $fluidEl.width();
+
+      // Resize all videos according to their own aspect ratio
+      $allVideos.each(function() {
+
+        var $el = $(this);
+        $el
+          .width(newWidth)
+          .height(newWidth * $el.data('aspectRatio'));
+
+      });
+
+    // Kick off one resize to fix all videos on page load
+    }).resize();
+  };
+};
+
 loadEvents();
 loadProjects();
+loadTutorials();
