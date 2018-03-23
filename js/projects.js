2107604232b4484f27a36babbdad21f6ea4e9628
Guus Hoekman <mail@guushoekman.com>
2018-03-23 11:57:18 +0200

    add single project page with basic search

---
+++ js/projects.js
@@ -0,0 +1,28 @@
+function loadProjects() {
+  var projectsDoc = 'https://docs.google.com/spreadsheets/d/1HJBxoYSVZyPrMuXLa5rrk0_5FcMUA9I48B6gltXpbA4/pubhtml';
+
+  $(document).ready(function() {
+    Tabletop.init({
+      key: projectsDoc,
+      callback: showProjects,
+      parseNumbers: false
+    });
+  });
+
+  function showProjects(data, tabletop) {
+    var source = $("#projects-template").html();
+    var template = Handlebars.compile(source);
+
+    $.each( tabletop.sheets("Projects").all(), function(i, detail) {
+      var html = template(detail);
+      $("#projects").append(html);
+    });
+
+    $('#search-highlight').hideseek({
+      highlight: true
+    });
+
+  };
+};
+
+loadProjects();
