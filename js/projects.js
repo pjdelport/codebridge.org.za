a3f0af1154d1cb26a04ef7f358f88fc0ba184bbd
Guus Hoekman <mail@guushoekman.com>
2018-03-28 11:14:42 +0200

    projects with better logic for empty fields

--- js/projects.js
+++ js/projects.js
@@ -1,9 +1,9 @@
-function loadProjects() {
-  var projectsDoc = 'https://docs.google.com/spreadsheets/d/1HJBxoYSVZyPrMuXLa5rrk0_5FcMUA9I48B6gltXpbA4/pubhtml';
+var contentDoc = 'https://docs.google.com/spreadsheets/d/1Wc7hkoh0T32zDRtcJIVGw1pKqTjHASAlj92vz6Qz5zs/pubhtml';
 
+function loadProjects() {
   $(document).ready(function() {
     Tabletop.init({
-      key: projectsDoc,
+      key: contentDoc,
       callback: showProjects,
       parseNumbers: false
     });
